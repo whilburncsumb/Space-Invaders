68c4111537b88ae92b66726f55a17bf7aff15e5e
William <whilburn@csumb.edu>
2024-02-29 16:46:45 -0800

    Initial commit

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public int points = 20;
+    public delegate void EnemyDeath(int pointValue);
+    public static event EnemyDeath OnEnemyDeath;
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+      Debug.Log("Ouch!");
+      Destroy(collision.gameObject);
+      OnEnemyDeath.Invoke(points);
+      Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+  public GameObject bullet;
+
+  public Transform shottingOffset;
+
+  private void Start()
+  {
+    Enemy.OnEnemyDeath += EnemyOnOnEnemyDeath;
+
+    void EnemyOnOnEnemyDeath(int pointValue)
+    {
+      Debug.Log("Players recieved EnemyDeath, Points: " + pointValue);
+    }
+  }
+
+  // Update is called once per frame
+    void Update()
+    {
+      if (Input.GetKeyDown(KeyCode.Space))
+      {
+        GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+        Debug.Log("Bang!");
+
+        Destroy(shot, 3f);
+
+      }
+    }
+}

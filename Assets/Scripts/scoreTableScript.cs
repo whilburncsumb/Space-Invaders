e6bd76382922dfd1f1214d1a39e178246809bfd2
William <whilburn@csumb.edu>
2024-03-02 09:54:31 -0800

    Added score table screen

---
+++ Assets/Scripts/scoreTableScript.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using TMPro;
+using UnityEditor.U2D;
+using UnityEngine.TextCore.Text;
+using UnityEngine.U2D;
+
+public class SpriteText : MonoBehaviour
+{
+    void Start()
+    {
+        string text = $"SCORE: 0000\tHIGH SCORE: 0000\n" +
+                      $"\n" +
+                      $"*SCORE ADVANCE TABLE*\n" +
+                      $"<sprite=0> =? MYSTERY\n" +
+                      $"<sprite=1>=30 POINTS\n" +
+                      $"<sprite=2>=20 POINTS\n" +
+                      $"<sprite=3>=10 POINTS"; 
+
+        GetComponent<TextMeshProUGUI>().text = text;
+    }
+}
